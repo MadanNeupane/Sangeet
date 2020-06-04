e5297fa4261d03851ed0e7e3025c6bad7bdfe33a
Madan Neupane <madanneupane09@gmail.com>
2020-06-04 19:18:35 +0545

    updated with file access permission features

--- app/src/main/java/com/example/sangeet/MainActivity.java
+++ app/src/main/java/com/example/sangeet/MainActivity.java
@@ -1,14 +1,95 @@
 package com.example.sangeet;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean checkPermission = false;
+    Uri uri;
+    String songName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.custom_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.nav_upload){
+            if (validatePermission()) {
+                pickSong();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void pickSong() {
+        Intent intent_upload = new Intent();
+        intent_upload.setType("audio/*");
+        intent_upload.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent_upload, requestCode: 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==1){
+            if(resultCode==RESULT_OK){
+                uri = data.getData();
+                Cursor mcursor = getApplicationContext().getContentResolver()
+                        .query(uri, null, null, null);
+                int indexname = mcursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                mcursor.moveToFirst();
+                mcursor.getString(indexname);
+                mcursor.close();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private boolean validatePermission(){
+        Dexter.withActivity(MainActivity.this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        checkPermission = true;
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        checkPermission = false;
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+
+        return  checkPermission;
     }
 }
